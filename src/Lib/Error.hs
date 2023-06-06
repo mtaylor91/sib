ff593b00f347d39bb96031b9a54bf6151a4fb560
Mike Taylor <mike.charles.taylor@gmail.com>
2023-06-06 13:46:44 -0600

    Remove dead code

--- src/Lib/Error.hs
+++ src/Lib/Error.hs
@@ -1,9 +1,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Lib.Error
   ( BuildException(..)
-  , buildIOError
   , catchFail
-  , catchFailIO
   ) where
 
 import Control.Exception
@@ -23,19 +21,8 @@ data BuildException
 instance Exception BuildException
 
 
-buildIOError :: IOException -> BuildException
-buildIOError = BuildIOException
-
-
 catchFail :: State -> IO State -> IO State
 catchFail state action = do
   catch action $ \(e :: BuildException) -> do
-    putStrLn $ "  Caught exception: " ++ show e
-    pure $ state { failed = True }
-
-
-catchFailIO :: State -> IO State -> IO State
-catchFailIO state action = do
-  catch action $ \(e :: IOException) -> do
     putStrLn $ "  Caught exception: " ++ show e
     pure $ state { failed = True }
